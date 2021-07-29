9a7176705f5d85fbd950a5e7bc54730b81dcbcf3
macong0503 <1512346520@qq.com>
2021-07-29 20:00:20 +0800

    Create RecursionDemo02.java

---
+++ Test/RecursionTest/RecursionDemo02.java
@@ -0,0 +1,14 @@
+package com.hqyj.mc.Test.RecursionTest;
+
+public class RecursionDemo02 {
+    public static int sum(int n){
+        if (n==1){
+            return 1;
+        }
+        return n*sum(n-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+}
